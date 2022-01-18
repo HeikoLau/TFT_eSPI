c2ecf488b118a4ccbc32c2284186320b34ea25c0
Heiko Lau <h.lau@ifam-erfurt.de>
2022-01-18 16:30:21 +0100

    Start SSD2119 driver

---
+++ TFT_Drivers/SSD2119_Defines.h
@@ -0,0 +1,96 @@
+// Change the width and height if required (defined in landscape mode)
+// or use the constructor to over-ride defaults
+#ifndef TFT_WIDTH
+  #define TFT_WIDTH  320
+#endif
+#ifndef TFT_HEIGHT
+  #define TFT_HEIGHT 240
+#endif
+
+#define WRITE_COMMAND_16        // Change TFT_eSPI::writecommand(); to use tft_Write_16(c);
+
+// Generic commands
+#define TFT_NOP     0x00
+#define TFT_SWRST   0x00
+
+
+#define TFT_CASET 0
+
+#define TFT_CASET1     SSD2119_HorizontalRAMaddressStartPosition
+#define TFT_CASET2     SSD2119_HorizontalRAMaddressEndPosition
+
+#define TFT_PASET0     SSD2119_VerticalRAMaddressPosition
+
+#define TFT_RAM_ADDR1  SSD2119_SetGDDRAMx
+#define TFT_RAM_ADDR2  SSD2119_SetGDDRAMy
+
+#define TFT_RAMWR      SSD2119_RAMdataWrite
+
+#define TFT_MAD_BGR 0x10
+#define TFT_MAD_RGB 0x00
+
+#ifdef TFT_RGB_ORDER
+  #if (TFT_RGB_ORDER == 1)
+    #define TFT_MAD_COLOR_ORDER TFT_MAD_RGB
+  #else
+    #define TFT_MAD_COLOR_ORDER TFT_MAD_BGR
+  #endif
+#else
+  #define TFT_MAD_COLOR_ORDER TFT_MAD_BGR
+#endif
+
+
+// Not used
+#define TFT_INVOFF  0x00
+#define TFT_INVON   0x00
+#define TFT_RAMRD   0x00
+#define TFT_IDXRD   0x00
+
+/* SSD2119 Registers */
+#define SSD2119_OscillationStart                  0x00
+#define SSD2119_DriverOutputControl               0x01
+#define SSD2119_LCDdriveACcontrol                 0x02
+#define SSD2119_PowerControl1                     0x03
+#define SSD2119_DisplayControl                    0x07
+#define SSD2119_FrameCycleControl                 0x0b
+#define SSD2119_PowerControl2                     0x0c
+#define SSD2119_PowerControl3                     0x0d
+#define SSD2119_PowerControl4                     0x0e
+#define SSD2119_GateScanStartPosition             0x0f
+#define SSD2119_SleepMode1                        0x10
+#define SSD2119_EntryMode1                        0x11
+#define SSD2119_SleepMode2                        0x12
+#define SSD2119_EntryMode2                        0x15
+#define SSD2119_HorizontalPorch                   0x16
+#define SSD2119_VerticalPorch                     0x17
+#define SSD2119_PowerControl5                     0x1e
+#define SSD2119_Uniformity                        0x20
+#define SSD2119_RAMdataWrite                      0x22
+#define SSD2119_FrameFrequency                    0x25
+#define SSD2119_AnalogueSetting                   0x26
+#define SSD2119_VCOMOTP1                          0x28
+#define SSD2119_VCOMOTP2                          0x29
+#define SSD2119_Ycontrol01                        0x30
+#define SSD2119_Ycontrol02                        0x31
+#define SSD2119_Ycontrol03                        0x32
+#define SSD2119_Ycontrol04                        0x33
+#define SSD2119_Ycontrol05                        0x34
+#define SSD2119_Ycontrol06                        0x35
+#define SSD2119_Ycontrol07                        0x36
+#define SSD2119_Ycontrol08                        0x37
+#define SSD2119_Ycontrol09                        0x3a
+#define SSD2119_Ycontrol10                        0x3b
+#define SSD2119_VerticalScroll1                   0x41
+#define SSD2119_VerticalScroll2                   0x42
+#define SSD2119_VerticalRAMaddressPosition        0x44
+#define SSD2119_HorizontalRAMaddressStartPosition 0x45
+#define SSD2119_HorizontalRAMaddressEndPosition   0x46
+#define SSD2119_FirstWindowStart                  0x48
+#define SSD2119_FirstWindowEnd                    0x49
+#define SSD2119_SecondWindowStart                 0x4a
+#define SSD2119_SecondWindowEnd                   0x4b
+#define SSD2119_SetGDDRAMx                        0x4e
+#define SSD2119_SetGDDRAMy                        0x4f
+
+// Delay between some initialisation commands
+#define TFT_INIT_DELAY 0x00 // Not used unless commandlist invoked
---
+++ TFT_Drivers/SSD2119_Init.h
@@ -0,0 +1,45 @@
+
+// This is the command sequence that initialises the SSD2119 driver
+
+{ 
+  delay(500);
+  
+  writecommand(SSD2119_VCOMOTP1);                          writedata(0x00); writedata(0x06); // VCOM OTP
+  writecommand(SSD2119_OscillationStart);                  writedata(0x00); writedata(0x01); // start Oscillator
+  writecommand(SSD2119_SleepMode1);                        writedata(0x00); writedata(0x00); // Sleep mode
+  writecommand(SSD2119_DriverOutputControl);               writedata(0x72); writedata(0xef); // 0x72EF Driver Output Control
+  writecommand(SSD2119_LCDdriveACcontrol);                 writedata(0x06); writedata(0x00); // LCD Driving Waveform Control
+  writecommand(SSD2119_PowerControl1);                     writedata(0x6a); writedata(0x38); // Power Control 1
+  writecommand(SSD2119_EntryMode1);                        writedata(0x68); writedata(0x70); // 0x6874 Entry Mode
+  writecommand(SSD2119_GateScanStartPosition);             writedata(0x00); writedata(0x00); // Gate Scan Position
+  writecommand(SSD2119_FrameCycleControl);                 writedata(0x53); writedata(0x08); // Frame Cycle Control
+  writecommand(SSD2119_PowerControl2);                     writedata(0x00); writedata(0x03); // Power Control 2
+  writecommand(SSD2119_PowerControl3);                     writedata(0x00); writedata(0x0a); // Power Control 3
+  writecommand(SSD2119_PowerControl4);                     writedata(0x2e); writedata(0x00); // Power Control 4
+  writecommand(SSD2119_PowerControl5);                     writedata(0x00); writedata(0xb7); // 0x00be Power Control 5
+  writecommand(SSD2119_FrameFrequency);                    writedata(0x80); writedata(0x00); // Frame Frequency Control
+  writecommand(SSD2119_AnalogueSetting);                   writedata(0x38); writedata(0x00); // 0x7800 Analog setting
+  writecommand(0x27);                                      writedata(0x00); writedata(0x78); // Critical setting to avoid pixel defect
+  writecommand(SSD2119_SetGDDRAMx);                        writedata(0x00); writedata(0x00); // Ram Address Set
+  writecommand(SSD2119_SetGDDRAMy);                        writedata(0x00); writedata(0x00); // Ram Address Set
+  writecommand(SSD2119_SleepMode2);                        writedata(0x0d); writedata(0x99); // 0x08d9 Sleep mode
+  
+  writecommand(SSD2119_VerticalRAMaddressPosition);        writedata(0xef); writedata(0x00); // Vertical RAM address position
+  writecommand(SSD2119_HorizontalRAMaddressStartPosition); writedata(0x00); writedata(0x00); // Horizontal RAM start address position
+  writecommand(SSD2119_HorizontalRAMaddressEndPosition);   writedata(0x01); writedata(0xef); // Horizontal RAM end address position (wrap after it)
+  
+  writecommand(SSD2119_Ycontrol01);                        writedata(0x00); writedata(0x00); // Adjust the Gamma Curve
+  writecommand(SSD2119_Ycontrol02);                        writedata(0x01); writedata(0x04);
+  writecommand(SSD2119_Ycontrol03);                        writedata(0x01); writedata(0x00);
+  writecommand(SSD2119_Ycontrol04);                        writedata(0x03); writedata(0x05);
+  writecommand(SSD2119_Ycontrol05);                        writedata(0x05); writedata(0x05);
+  writecommand(SSD2119_Ycontrol06);                        writedata(0x03); writedata(0x05);
+  writecommand(SSD2119_Ycontrol07);                        writedata(0x07); writedata(0x07);
+  writecommand(SSD2119_Ycontrol08);                        writedata(0x03); writedata(0x00);
+  writecommand(SSD2119_Ycontrol09);                        writedata(0x12); writedata(0x00);
+  writecommand(SSD2119_Ycontrol10);                        writedata(0x08); writedata(0x00);
+  
+  writecommand(SSD2119_DisplayControl);                    writedata(0x00); writedata(0x33); // Display Control
+  
+  delay(150);
+}
